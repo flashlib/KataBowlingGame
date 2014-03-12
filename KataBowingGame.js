a017a731b75e6664fe2359f5655cd4bc23c1935e
Jacky <flashlib@gmail.com>
2014-03-12 16:47:57 +0800

    重构代码，未通过测试。by 刘志

--- KataBowingGame.js
+++ KataBowingGame.js
@@ -1,14 +1,31 @@
 
 var  KataBowlingGame = {
 	  totalScore:0,
+	  prevScore:0,
+	  prevPrevScore:0,
+	  num : 1,
 	  score:function() {
 	 	return this.totalScore;
 	 },
-	  roll:function (score){
-	  	if(score==10){
-	  		this.totalScore = 300;
-	  	}else if(score == 0){
-	  		this.totalScore = 0;
+	 init:function (){
+	 	this.totalScore = 0;
+	 	this.prevScore = 0;
+	 	this.prevPrevScore = 0;
+	 	this.num = 1;
+	 },
+	 
+	 roll:function (score){
+	  	this.totalScore = this.totalScore + score;
+	  	if (this.prevScore == 10 && this.num <= 10) {
+	  		this.totalScore = this.totalScore + score;
 	  	}
+	  	
+	  	if (this.prevPrevScore == 10 && this.num <= 10) {
+	  		this.totalScore = this.totalScore + score;
+	  	}
+	  	
+	  	this.prevPrevScore = this.prevScore;
+	  	this.prevScore = score;
+	  	this.num = this.num + 1;
 	 } 
 };
--- test.js
+++ test.js
@@ -4,6 +4,7 @@
 
 test("all strike", function(){
 	var game = KataBowlingGame;
+	game.init();
 	game.roll(10);
 	game.roll(10);
 	game.roll(10);
@@ -14,12 +15,14 @@ test("all strike", function(){
 	game.roll(10);
 	game.roll(10);	
 	game.roll(10);
-	
-	ok(300==game.score(), "all strike should equal to 300!");
+	game.roll(10);
+	game.roll(10);
+	ok(300==game.score(), "all strike should equal to 300! Current Value is: " + game.score());
 });
 
 test("all false", function(){
 	var game = KataBowlingGame;
+	game.init();
 	game.roll(0);
 	game.roll(0);
 	game.roll(0);
@@ -30,6 +33,15 @@ test("all false", function(){
 	game.roll(0);
 	game.roll(0);	
 	game.roll(0);
-	
-	ok(0==game.score(), "all false should equal to 0!");
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);	
+	game.roll(0);	
+	ok(0==game.score(), "all false should equal to 0! Current Value is: " + game.score());
 });

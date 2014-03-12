99d330a3b21a83b5b07ed32205e50eca5c4227bf
Jacky <flashlib@gmail.com>
2014-03-12 14:49:32 +0800

    TEST UNIT ALL STRIKE PASS BY 422578

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
-test("hello world", function(){
-	ok(1=="1", "1 should equal to '1'!");
+var  KataBowlingGame = {
+	  score:function() {
+	 	return 300;
+	 },
+	  roll:function (){
+	 	
+	 } 
+};
+
+test("all strike", function(){
+	var game = KataBowlingGame;
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);
+	game.roll(10);	
+	game.roll(10);
+	
+	ok(300==game.score(), "all strike should equal to 300!");
 });

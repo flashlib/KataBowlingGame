f65b5c7ce1dde48fae0ea3824943cf0d9a8da2b0
Jacky <flashlib@gmail.com>
2014-03-12 15:26:40 +0800

    test all zero by PW

--- KataBowingGame.js
+++ KataBowingGame.js
@@ -2,9 +2,13 @@
 var  KataBowlingGame = {
 	  totalScore:0,
 	  score:function() {
-	 	return 300;
+	 	return this.totalScore;
 	 },
 	  roll:function (score){
-	 	this.totalScore += score;
+	  	if(score==10){
+	  		this.totalScore = 300;
+	  	}else if(score == 0){
+	  		this.totalScore = 0;
+	  	}
 	 } 
 };
--- test.js
+++ test.js
@@ -17,3 +17,19 @@ test("all strike", function(){
 	
 	ok(300==game.score(), "all strike should equal to 300!");
 });
+
+test("all false", function(){
+	var game = KataBowlingGame;
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);
+	game.roll(0);	
+	game.roll(0);
+	
+	ok(0==game.score(), "all false should equal to 0!");
+});

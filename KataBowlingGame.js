9871eb3a63db364c3775a4763b28481a8f76808a
Jacky <flashlib@gmail.com>
2014-04-03 14:33:40 +0800

    CodingDojo3:Restart from draft.--Jacky

--- KataBowlingGame.js
+++ KataBowlingGame.js
@@ -1,31 +1,13 @@
+function KataBowlingGame()
+{
+	var obj = {
+	  score: function() {
+	    return 0;
+	  },
+	
+	  roll: function(score) {
+	  }
+	}; 
 
-var  KataBowlingGame = {
-	  totalScore:0,
-	  prevScore:0,
-	  prevPrevScore:0,
-	  num : 1,
-	  score:function() {
-	 	return this.totalScore;
-	 },
-	 init:function (){
-	 	this.totalScore = 0;
-	 	this.prevScore = 0;
-	 	this.prevPrevScore = 0;
-	 	this.num = 1;
-	 },
-	 
-	 roll:function (score){
-	  	this.totalScore = this.totalScore + score;
-	  	if (this.prevScore == 10 && this.num <= 10) {
-	  		this.totalScore = this.totalScore + score;
-	  	}
-	  	
-	  	if (this.prevPrevScore == 10 && this.num <= 10) {
-	  		this.totalScore = this.totalScore + score;
-	  	}
-	  	
-	  	this.prevPrevScore = this.prevScore;
-	  	this.prevScore = score;
-	  	this.num = this.num + 1;
-	 } 
-};
+	return obj;
+}
--- test.js
+++ test.js
@@ -1,47 +1,24 @@
-
-
-
-
-test("all strike", function(){
-	var game = KataBowlingGame;
-	game.init();
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);	
-	game.roll(10);
-	game.roll(10);
-	game.roll(10);
-	ok(300==game.score(), "all strike should equal to 300! Current Value is: " + game.score());
-});
-
-test("all false", function(){
-	var game = KataBowlingGame;
-	game.init();
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);	
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);
-	game.roll(0);	
-	game.roll(0);	
-	ok(0==game.score(), "all false should equal to 0! Current Value is: " + game.score());
+test("all 0", function() {
+  var game = new KataBowlingGame();
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  game.roll(0);
+  ok(0 == game.score(), "all 0 should equal to 0! Current Value is: " + game.score());
 });

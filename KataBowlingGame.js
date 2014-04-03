35b827efb7597e99f537b6c60fc1b8891f43225a
Jacky <flashlib@gmail.com>
2014-04-03 15:10:37 +0800

    refactor-Chrystal

--- KataBowlingGame.js
+++ KataBowlingGame.js
@@ -1,18 +1,29 @@
 function KataBowlingGame()
 {
 	var obj = {
+		balls: new Array(),
 		scores: new Array(),
 	  score: function() {
 	  	var total = 0;
-		  for(var i=0; i < this.scores.length; i++)
+	  	this.calculate();
+	  	for(var i=0; i < this.scores.length; i++)
 		  {
 		  	total += this.scores[i];
 		  }
+		  	
+		  
 		  return total; 	  	
 	  },
-	
+	  
+	  calculate: function(){
+	  	for(var i=0; i < this.balls.length; i++)
+		  {
+		  	this.scores.push(this.balls[i]);
+		  }
+	  },
+	  
 	  roll: function(score) {
-	  	this.scores.push(score);
+	  	this.balls.push(score);
 	  }
 	}; 
 	

ceb23cad1849b0ec629cd3417861eed0ef1b8ec4
Jacky <flashlib@gmail.com>
2014-04-03 16:33:29 +0800

    xulingbo 用新类实现前2个测试用例。

--- KataBowlingGame.js
+++ KataBowlingGame.js
@@ -2,25 +2,24 @@ function KataBowlingGame()
 {
 	var obj = {
 		balls: new Array(),
-		scores: new Array(),
+		frames: new Array(),
 	  score: function() {
 	  	var total = 0;
 	  	this.calculate();
-	  	for(var i=0; i < this.scores.length; i++)
+	  	for(var i=0; i < this.frames.length; i++)
 		  {
-		  	total += this.scores[i];
+		  	total += this.frames[i].score();
 		  }
 	
 		  return total; 	  	
 	  },
 	  
 	  calculate: function(){
-	  	this.scores = new Array();
+	  	this.frames = new Array();
 
 	  	for(var i=0; i < this.balls.length; i++)
 		  {	
-
-		  	this.scores.push(this.balls[i]);
+				 this.frames.push(Frame(this.balls[i],this.balls[i++]));
 		  }		  
 	  },
 	  
@@ -32,24 +31,29 @@ function KataBowlingGame()
 	return obj;
 }
 
-function  MakeFrames(balls){
-		
-		var frames = new Array() ;
-		for (var i = 0; i < balls.length; i++) {
-				if (balls[i] === 10) {
-						frames.push(new Frame(balls[i], 999)) ;								
-				}
-				else {
-						frames.push(new Frame(balls[i],balls[i++])) ;					
-				}
-		}
-		return frames ;
-}
+// function  MakeFrames(balls){
+// 		
+		// var frames = new Array() ;
+		// for (var i = 0; i < balls.length; i++) {
+				// if (balls[i] === 10) {
+						// frames.push(new Frame(balls[i], 999)) ;								
+				// }
+				// else {
+						// frames.push(new Frame(balls[i],balls[i++])) ;					
+				// }
+		// }
+		// return frames ;
+// }
 
 function Frame(first, sec) {
-		var frame = new Array() ;
-		frame = [first, sec] ;
-		return frame ;
+
+  var obj = {
+    frame: [first, sec],
+    score: function() {
+      return this.frame[0] + this.frame[1];
+    }
+  };
+  return obj;
 }
 
 
